23dfcda9e62401b51c0312c65bbbccfc4865faf5
William F. Awuku Jr <33478637+sparkserian@users.noreply.github.com>
2021-11-15 22:07:07 -0500

    Create currency-inthe-thousands.py
    print the number 150000 as currency with thousands grouped with commas. Currency should be displayed with two decimal places

---
+++ currency-inthe-thousands.py
@@ -0,0 +1,7 @@
+#print the number 150000 as currency with thousands grouped with commas
+#currency should be displayed with two decimal places
+
+def main():
+    print("$", format(150000, ',.2f'))
+
+main()
